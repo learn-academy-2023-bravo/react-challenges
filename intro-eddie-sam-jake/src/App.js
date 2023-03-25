817762aab5091b7fc5a6f026be1258784143e1e6
EddieCarrion.github.io <ecar25L@gmail.com>
2023-03-24 17:03:45 -0700

    progress

---
+++ intro-eddie-sam-jake/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Components/Header"
+import Profile from "./Components/Profile"
+function App() {
+  return (
+   <>
+    <Header />
+<Profile />
+   </>
+  );
+}
+
+export default App;
