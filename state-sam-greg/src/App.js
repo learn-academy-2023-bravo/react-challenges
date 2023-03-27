a3540982da25eb59cb7e520480505e47732b42f3
sam-saenz <samsaenz31@gmail.com>
2023-03-27 15:14:14 -0700

    Did first part of the challenge

---
+++ state-sam-greg/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Box from "./components/Box"
+import "./App.css"
+
+const App = () => {
+  return(
+    <>
+      <h1>Color Box Application</h1>
+      <Box />
+      {/* <Box /> */}
+    </>
+  )
+}
+
+export default App;
