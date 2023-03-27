1d528b5a007208f565ccd2d056b71b38039f30d4
FLopez73 <flopez730@gmail.com>
2023-03-27 15:57:07 -0700

    colorbox 2

--- state-wagner-felix/src/App.js
+++ state-wagner-felix/src/App.js
@@ -1,33 +1,17 @@
-import React, { useState } from "react"
+import React from "react"
+import ColorBox from "./components/ColorBox"
 
 const App = () => {
-  const [color, setColor] = useState("white")
-
-  const handleClick = () => {
-    const colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"]
-    const randomColor = Math.floor(Math.random() * colors.length)
-    setColor(colors[randomColor])
-  }
-
   return (
-    <div
-      style={{
-        width: "400px",
-        height: "400px",
-        backgroundColor: color,
-        border: "5px solid black",
-        padding: "20px",
-        textAlign: "center",
-        alignItems: "center"
-         }}
-    >
-      <h1>ColorBox</h1>
-      <button onClick={handleClick}>Change Color</button>
-      <p>{color}</p>
+    <>
+      <ColorBox />
 
-    </div>
+      <ColorBox />
 
-    
+      <ColorBox />
+
+      <ColorBox />
+    </>
   )
 }
 
