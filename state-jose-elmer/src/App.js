64283287d34cb02c2483512b4d19d894556eb48c
drfudd2001 <elmer.figueroagarcia@gmail.com>
2023-03-27 16:52:44 -0700

    The box is done.

--- state-jose-elmer/src/App.js
+++ state-jose-elmer/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [boxColor, setBoxColor] = useState('#ffffff')
+  const [colorName, setColorName] = useState('White')
+
+  const changeColor = () => {
+    const colors = [
+      { name: 'Red', value: '#ff0000' },
+      { name: 'Green', value: '#00ff00' },
+      { name: 'Blue', value: '#0000ff' },
+      { name: 'Yellow', value: '#ffff00' },
+      { name: 'Magenta', value: '#ff00ff' }
+    ]
+    const randomColor = colors[Math.floor(Math.random() * colors.length)]
+    setBoxColor(randomColor.value)
+    setColorName(randomColor.name)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div
+        className="color-box"
+        style={{ backgroundColor: boxColor }}
+        onClick={changeColor}
+      >
+        <div className="message">Click me!</div>
+        {/* Add a new div to display the color name */}
+        <div className="color-name">{colorName}</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
