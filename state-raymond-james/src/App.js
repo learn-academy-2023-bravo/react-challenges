6658d215c4431fcae6e532167219ae4b5f411919
jameshlee1 <james.h.lee3@outlook.com>
2023-03-27 15:20:01 -0700

    Finished React State Stretch Gaals

--- state-raymond-james/src/App.js
+++ state-raymond-james/src/App.js
@@ -1,33 +1,55 @@
 import logo from './logo.svg'
 import './App.css'
+// import { useState } from 'react'
+import ColorBox from './components/ColorBox'
 import { useState } from 'react'
 
-const boxStyles = {
-	border: '1px solid black',
-	backgroundColor: 'white',
-	width: '100px',
-	height: '100px',
-	cursor: 'pointer',
-}
+// const boxStyles = {
+// 	border: '1px solid black',
+// 	width: '100px',
+// 	height: '100px',
+// 	cursor: 'pointer',
+// }
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink']
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink']
 
 function App() {
-	const [color, setColor] = useState('white')
+	// const [color, setColor] = useState('white')
 
-	const handleClick = () => {
-		console.log('clicked')
-	}
-
+	// const handleClick = () => {
+	// setColor(colors[Math.floor(Math.random() * (colors.length))])
+	// }
+const [numberOfBoxes, setNumberOfBoxes] = useState(0)
 	return (
 		<>
 			<h1>Hello, please click on the box</h1>
+	{/* <ColorBox/> 
+	<ColorBox/> 
+	<ColorBox/> */}
+	{new Array(numberOfBoxes).fill('').map((_, index) => <ColorBox key={index} />)}
 
-			<button onClick={handleClick} style={boxStyles}>
+
+	<button onClick={()=>{
+		setNumberOfBoxes(numberOfBoxes + 1)
+	}}>Add</button>
+
+{numberOfBoxes > 0 &&(
+	<button onClick={() =>{
+		setNumberOfBoxes(numberOfBoxes - 1)
+	}}>Remove</button>
+)}
+	
+
+			{
+			
+			/* <button onClick={handleClick} style={{...boxStyles, backgroundColor: color }}>
 				{color}
-			</button>
-		</>
+			</button> */}
+			</>
+
 	)
 }
+
+
 
 export default App
