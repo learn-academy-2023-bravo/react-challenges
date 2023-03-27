59a26698bedcb522909e3489c4d06c024357d243
salas.89jl <salas.89jl@gmail.com>
2023-03-27 13:45:01 -0700

    started some of the code blocks

---
+++ state-elmer-jose/src/components/Color.js
@@ -0,0 +1,42 @@
+// imports 
+import React, {useState} from "react"
+
+// function component declaration
+const Color = () => {
+
+    // add state above the return statement
+    const [color, setColor] = useState("white")
+
+    // array of collors
+    const colorScheme = [
+    "red", 
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "pink"
+    ]
+
+    // custom function to draw random colors
+    const colorHandle= () => {
+    const randomColor = Math.floor(Math.random() * colorScheme.length)
+    setColor(colorScheme[randomColor])
+    }
+    
+    return(
+        <>
+            <p>Color: {color} </p>
+            <button
+                onClick={colorHandle}
+                style={{
+                    backgroundColor: "white",
+                    color: "black"
+                }}
+                >
+                    Click for new color
+                </button>
+        </>
+    )
+}
+export default Color
