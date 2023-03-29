cabdabd2a8336185dd36654adf5f562c25022ec7
dmooredidit <dandre.moore2@gmail.com>
2023-03-29 11:58:48 -0700

    placed buttong in first div with border

---
+++ props-dandre-felix-greg/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react' 
+import "./App.css" 
+import Bones from "./Components/Bones"
+
+
+
+const App = () => {
+  return(
+    <>
+    
+    <h1>Throwin' Bones 🦴</h1>
+  <div className='card'>
+    <div className='button'>
+        <button>Click to Roll Die</button>
+
+    </div>
+
+   </div> 
+    </>
+  )
+}
+export default App;
---
+++ props-dandre-felix-greg/src/Components/Bones.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Bones = () => {
+    return(
+      <>      
+      <div className='button'>
+          {/* <button>Click To Roll Die</button>   */}
+      </div>
+      </>
+  
+      
+    )
+  }
+  export default Bones
