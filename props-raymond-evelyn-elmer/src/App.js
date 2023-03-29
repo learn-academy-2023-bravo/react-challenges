d7e0b5cb27900d0ba0c0d52ce4acc3389bdd04c8
drfudd2001 <elmer.figueroagarcia@gmail.com>
2023-03-29 14:40:46 -0700

    Just trying to get the dice rolling.

--- props-raymond-evelyn-elmer/src/App.js
+++ props-raymond-evelyn-elmer/src/App.js
@@ -1,12 +1,20 @@
+// Import the CSS file for styling
 import './App.css'
+
+// Import the Dice component
 import Dice from './components/Dice'
 
+// Define the App component
 function App() {
+	// Render the component
 	return (
-		<div className="App">
+		// Render the container div with the class "App"
+		// Render the Dice component inside the container div
+		<div className="App">			
 			<Dice />
 		</div>
 	)
 }
 
+// Export the App component as the default export
 export default App
--- props-raymond-evelyn-elmer/src/components/Dice.js
+++ props-raymond-evelyn-elmer/src/components/Dice.js
@@ -1,12 +1,35 @@
-import React from 'react'
+// Import the required functions and components from React
+import React, { useState } from 'react'
 
+// Define a function-based component for the dice
 const Dice = () => {
+
+	// Define a state variable for the number inside the dice, initialized to 1
+	const [number, setNumber] = useState(1)
+
+	// Define a function to handle the click event
+	function handleClick() {
+	
+		// Generate a random number between 1 and 6
+		const newNumber = Math.floor(Math.random() * 6) + 1
+
+		// Update the state variable with the new number
+	  	setNumber(newNumber)
+	}
+
+	// Render the component
 	return (
-		<div className="dice-container">
-			<div id="dice">4</div>
+
+		// Render the container div with the click event handler
+		// Render the div for the number inside the dice
+		// Render the message inviting the user to click the box to roll the dice]		
+		
+		<div className="dice-container" onClick={handleClick}>
+			<div id="dice">{number}</div>
 			<h1>Click Box to Roll</h1>
 		</div>
 	)
 }
 
+// Export the component as the default export
 export default Dice
