10c06f9872252e74d8d98e89d7a2efb31dfc3f49
raymondkneipp <raymondkneipp@gmail.com>
2023-03-29 11:35:40 -0700

    first challenge bullet done

---
+++ props-raymond-evelyn-elmer/src/components/Dice.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dice = () => {
+	return (
+		<div className="dice-container">
+			<div id="dice">4</div>
+			<h1>Click Box to Roll</h1>
+		</div>
+	)
+}
+
+export default Dice
