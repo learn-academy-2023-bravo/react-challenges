8d36037c142ede32a9f228715d909e848a32caf6
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-03-29 16:42:50 -0700

    Dice roll and log complete

---
+++ props-jake-eddie-valeria/src/components/Die.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+const Die = (props) => {
+console.log(props)
+  return (
+    <>
+      <h1 className="dice">{props.dice}</h1>
+      
+    </>
+  )
+
+}
+
+
+export default Die
---
+++ props-jake-eddie-valeria/src/components/Log.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Log = (props) => {
+    console.log(props)
+      return (
+        <>
+          <h1 className="log">{props.log}</h1>
+          
+        </>
+      )
+    
+    }
+
+export default Log
