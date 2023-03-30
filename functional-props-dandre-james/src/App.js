068f5c6a9285feb4e205de4bf99aacdfd119f31e
dmooredidit <dandre.moore2@gmail.com>
2023-03-30 16:44:21 -0700

    completed cookout site

--- functional-props-dandre-james/src/App.js
+++ functional-props-dandre-james/src/App.js
@@ -1,33 +1,45 @@
 import { useState } from "react"
+import MenuItem from "./Components/MenuItem"
+import "./App.css"
+
 
 const App = () => {
-const [menu, orderItem] = useState ([
-  { name: "Steak", ordered: false },
-  { name: "Burgers", ordered: false },
-  { name: "Meat Loaf", ordered: false },
-  { name: "Spicy Chicken Wings", ordered: false },
-  { name: "Henn Dog", ordered: false },
-  { name: "Jack & Coke", ordered: false },
-  { name: "Salad", ordered: false }
-])
+  const [menu, setMenu] = useState ([
+    { name: "Steak - $25.99", ordered: false },
+    { name: "Burgers - $15", ordered: false },
+    { name: "Meat Loaf - $13.49", ordered: false },
+    { name: "Spicy Chicken Wings - $9.99", ordered: false },
+    { name: "Henn Dog - $12", ordered: false },
+    { name: "Jack & Coke - $17", ordered: false },
+    { name: "Salad - FREE & UNLIMITED!!!", ordered: false }
+  ])
+ 
 
-const menuItem = (orderItem) => {
- menu[].ordered = true
-orderItem([...menu])
-}
-return (
-  <div className="container">
-    
-    <div className='menu'>
-      <h1>Dandre and Jame's Throwdown on the Grill</h1>
-      <h2>Awesome Menu</h2>
+  const orderedItems = (selectedFood) => {
+    menu[selectedFood].ordered = true
+    setMenu([...menu])
+  }
+  return (
+    <div className="container">
+        
+      <div className='menu'>
+        <h1>D'Andre and James' Throwdown on the Grill</h1>
+        <h2>Awesome Menu</h2>
 
-
-
-
+        {menu.map((selectedFood, index) => {
+      
+          return (
+            <MenuItem
+            orderedItems={orderedItems}
+            key={index}
+            index={index}
+            selectedFood={selectedFood}
+            />
+          )
+        })}
+      </div>
     </div>
-    </div>
-)
+  )
 }
 
 export default App
