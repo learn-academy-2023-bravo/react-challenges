dccba5a2395db543c0213e238bac3b3a88aa177d
raymondkneipp <raymondkneipp@gmail.com>
2023-03-30 16:36:55 -0700

    stretch goals

--- functional-props-raymond-greg/src/components/Menu.js
+++ functional-props-raymond-greg/src/components/Menu.js
@@ -3,12 +3,27 @@
 //each menu item will have a button, when pressed it will call a function inside app.js
 
 const Menu = (props) => {
-return(
-    <>
-    <h1>Menu</h1>
-    {props.data.map((food, index) => {
-        return <p key={index}>{food.name}-${food.price} <button onClick={()=> props.handleClick(food)}>Order </button></p>
-    })}
-    </>)
+	return (
+		<>
+			<h1>Menu</h1>
+			{props.data.map((food, index) => {
+				return (
+					<div
+						className={`food-item ${food.special === true ? 'special' : ''}`}
+						key={index}
+					>
+						<div className="heading">
+							<div>
+								<h3>{food.name}</h3>
+								<p>${food.price}</p>
+							</div>
+							<button onClick={() => props.handleClick(food)}>Order </button>
+						</div>
+						<img src={food.image} />
+					</div>
+				)
+			})}
+		</>
+	)
 }
 export default Menu
