bbdeeb457fb1ff0bce79d20ba476e9bce10d7eb8
borja-x13 <bxaire13@gmail.com>
2023-03-31 12:19:26 -0700

    inputs challenge up to good robot working

---
+++ inputs-borja-mark/src/components/BadRobot.js
@@ -0,0 +1,12 @@
+//create a function that is going to call the prop Good robot
+
+
+const BadRobot = ({ badRobotMachine }) => {
+    return (
+        <>
+          {badRobotMachine}
+        </>
+    )
+}
+
+export default BadRobot
---
+++ inputs-borja-mark/src/components/GoodRobot.js
@@ -0,0 +1,14 @@
+
+//create a function that is going to call the prop Good robot
+// Returns string interpolation
+// `I hear you saying ${userText}. Is that correct? `
+
+const GoodRobot = ({ goodRobotMachine }) => {
+    return (
+        <>
+          {goodRobotMachine}
+        </>
+    )
+}
+
+export default GoodRobot
